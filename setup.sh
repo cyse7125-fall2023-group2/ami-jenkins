fe588209587b27a23e7e42abea5598b33da84f7f
Kammal Shetty Sai Sumanth <sumanthksai@gmail.com>
2023-10-24 14:07:39 -0400

    feat:added semvers

--- setup.sh
+++ setup.sh
@@ -1,6 +1,45 @@
 #!/bin/bash
 
 sudo apt update
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+
+echo "##################################################################" \
+     "####################INSTALLING Docker ##############################" \
+    "###################################################################"
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+
+echo "##################################################################" \
+     "####################INSTALLING NODE ##############################" \
+    "###################################################################"
+curl -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
+sudo bash /tmp/nodesource_setup.sh
+sudo apt install nodejs -y
+sudo node -v
+sudo npm install -g semantic-release@17.4.4
+sudo npm install -g @semantic-release/git@9.0.0
+sudo npm install -g @semantic-release/exec@5.0.0
+sudo npm install -g conventional-changelog-conventionalcommits
+sudo npm install -g npm-cli-login
+
+
+echo "##################################################################" \
+     "####################INSTALLING Jenkins ##############################" \
+    "###################################################################"
 sudo apt install openjdk-17-jre -y 
 
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
@@ -9,6 +48,9 @@ curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+  
+
+
 export JAVA_OPTS="-Djenkins.install.runSetupWizard=false"
 sudo apt-get update
 sudo apt-get install jenkins -y
@@ -25,4 +67,7 @@ sudo cp /tmp/jenkins.service /usr/lib/systemd/system/jenkins.service
 sudo cp /tmp/plugin.txt /usr/share/jenkins/plugins.txt
 
 sudo rm -rf /tmp/jenkins.service
+
+
+
 sudo systemctl daemon-reload
